f6133bf1bfbc8514989b45ed143f61d8b2e478ca
melissajimison <melissajimison@gmail.com>
2016-12-21 21:22:31 -0800

    sorts

---
+++ sort/merge.rb
@@ -0,0 +1,18 @@
+def merge_sort(array)
+  return array if array.length == 1
+  middle = array.length / 2
+  conquer(merge_sort(array[0...middle]), merge_sort(array[middle..-1]))
+end
+
+def conquer(left, right)
+  sorted = []
+   until left.empty? || right.empty?
+     if left.first <= right.first
+       sorted << left.shift
+     else
+       sorted << right.shift
+     end
+   end
+   sorted.concat(left).concat(right)
+end
+puts "the result #{merge_sort([7,4,43,54,6,6,3,235,76,8,35])}"
