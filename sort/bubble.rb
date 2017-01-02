b1b0f365b4dfba153f3c1a3a6ce99e8bd79f02cb
melissajimison <melissajimison@gmail.com>
2017-01-01 19:37:19 -0800

    insertion

--- sort/bubble.rb
+++ sort/bubble.rb
@@ -1,19 +1,17 @@
-def bubble_sort(list)
-  return list if list.size <= 1 # already sorted
-  iterations = 0
+def bubble_sort(array)
+  return array if array.size <= 1
 
   swapped = true
   while swapped
     swapped = false # maybe this time, we won't find a swap
-    for i in 0..list.size-2
-      if list[i] > list[i+1]
-        list[i], list[i+1] = list[i+1], list[i] # swap values
+    for i in 0..array.size-2
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i] # swap values
         swapped = true # found a swap... keep going
         iterations += 1
       end
     end
   end
-  puts iterations
-  list
+  array
 end
-puts "the result #{bubble_sort([3, 4, 6, 6, 7, 8, 35, 43, 54, 76, 235])}"
+puts "the result #{bubble_sort([7,4,43,54,6,6,3,235,76,8,35])}"
---
+++ sort/insertion.rb
@@ -0,0 +1,17 @@
+def intertion_sort(array)
+  return array if array.size <= 1
+  for i in 1..array.length - 1
+    element = array[i]
+    j = i
+    while j > 0 && array[j-1] > element
+      puts "array[j-1] #{array[j-1]}"
+      puts "array[j] #{array[j]}"
+      puts "element #{element}"
+      array[j] = array[j-1]
+      j = j - 1
+    end
+    array[j] = element
+  end
+  array
+end
+puts "the result #{intertion_sort([7,4,43,54,6,6,3,235,76,8,35])}"
